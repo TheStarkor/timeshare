cb416c6ae9c1cb39fb12351481daaf181c26191c
Jiseung <micko6420@gmail.com>
2022-03-07 02:37:49 +0900

    [style]signup 모바일

--- src/pages/Welcome/join.js
+++ src/pages/Welcome/join.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
@@ -17,6 +17,29 @@ const Signup = () => {
   const [isError, setError] = useState(false);
   const [imageUrl, setImageUrl] = useState("");
   const [form] = Form.useForm();
+  const [isMobile, setisMobile] = useState(false);
+
+  const resizingHandler = () => {
+    if (window.innerWidth <= 600) {
+      setisMobile(true);
+    } else {
+      setisMobile(false);
+    }
+  };
+
+  // 우선 맨 처음 1023이하면 모바일 처리
+  useEffect(() => {
+    if (window.innerWidth <= 600) {
+      setisMobile(true);
+      console.log(isMobile)
+    }
+
+    window.addEventListener("resize", resizingHandler);
+    return () => {
+      // 메모리 누수를 줄이기 위한 removeEvent
+      window.removeEventListener("resize", resizingHandler);
+    };
+  }, []);
 
   const onGenderChange = (value) => {
     switch (value) {
@@ -85,40 +108,47 @@ const Signup = () => {
           <div className="greeting-text1">
             회원가입
           </div>
-          <div className="greeting-text2">
-            프로필을 입력하고 업무, 채용, 커리어 이야기 등을 나눠보세요.
-          </div>
-
+          {isMobile ? <>
+            <div className="greeting-text2">
+            </div>
+          </>
+            : <>
+              <div className="greeting-text2">
+                프로필을 입력하고 업무, 채용, 커리어 이야기 등을 나눠보세요.
+              </div>
+            </>
+          }
           <div className="signup-form-container">
-            <Form initialValues={{ remember: true }} onFinish={onFinish}>
-              <Form.Item
-                name="image"
-                rules={[
-                  { required: true, message: "프로필 사진을 추가해주세요!" },
-                ]}
-              >
-                <Upload
+            {isMobile ? <>
+              <Form initialValues={{ remember: true }} onFinish={onFinish}>
+                <Form.Item
                   name="image"
-                  listType="picture-card"
-                  className="avatar-uploader"
-                  showUploadList={false}
-                  action={
-                    "http://ec2-13-125-111-9.ap-northeast-2.compute.amazonaws.com/uploads"
-                  }
-                  onChange={handleChange}
+                  rules={[
+                    { required: true, message: "프로필 사진을 추가해주세요!" },
+                  ]}
                 >
-                  {imageUrl ? (
-                    <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
-                  ) : (
-                    <div>
-                      <PlusOutlined style={{ color: "#ffffff70" }} />
-                      {/* <div style={{ marginTop: 8 , color:'#ffffff'}}>Profile</div> */}
-                    </div>
-                  )}
-                </Upload>
-              </Form.Item>
+                  <Upload
+                    name="image"
+                    listType="picture-card"
+                    className="avatar-uploader"
+                    showUploadList={false}
+                    action={
+                      "http://ec2-13-125-111-9.ap-northeast-2.compute.amazonaws.com/uploads"
+                    }
+                    onChange={handleChange}
+                  >
+                    {imageUrl ? (
+                      <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                    ) : (
+                      <div>
+                        <PlusOutlined style={{ color: "#00000050" }} />
+                        {/* <div style={{ marginTop: 8 , color:'#ffffff'}}>Profile</div> */}
+                      </div>
+                    )}
+                  </Upload>
+                </Form.Item>
 
-              {/* <Form.Item
+                {/* <Form.Item
                 label="Nickname"
                 name="nickname"
                 rules={[{ required: true, message: "닉네임을 입력해주세요!" }]}
@@ -126,102 +156,254 @@ const Signup = () => {
                 <Input />
               </Form.Item> */}
 
-              <div className="form-container">
-                <div className="form-name">
-                  <Form.Item
-                    label="이름"
-                    name="name"
-                    rules={[{ required: true, message: "이름을 입력해주세요!" }]}
-                  >
-                    <Input 
-                    placeholder="이름 입력"/>
-                  </Form.Item>
-                </div>
-                <div className="form-studentnumber">
-                  <Form.Item
-                    label="학번 / 학과"
-                    name="studentId"                    
-                    rules={[{ required: true, message: "학번을 입력해주세요!" }]}
-                  >
-                    <Input placeholder="20221111/전산학부"/>
-                  </Form.Item>
-                </div>
-              </div>
-
-              <div className="form-container">
-                <div className="form-email">
-                  <Form.Item
-                    label="Email"
-                    name="email"
-                    rules={[{ required: true, message: "이메일을 입력해주세요!" }]}
-                  >
-                    <Input 
-                    placeholder="이메일"/>
-                  </Form.Item>
-                </div>
-                <div className="form-gender">
-                  <Form.Item name="gender" label="Gender" rules={[{ required: true, message: "성별을 선택해주세요!" }]}>
-                    <Select
-                      placeholder="성별"
-                      onChange={onGenderChange}
-                      allowClear
+                <div className="form-container">
+                  <div className="form-name">
+                    <Form.Item
+                      label="이름"
+                      name="name"
+                      rules={[{ required: true, message: "이름을 입력해주세요!" }]}
                     >
-                      <Option value="male">남성</Option>
-                      <Option value="female">여성</Option>
-                      <Option value="other">기타</Option>
-                    </Select>
-                  </Form.Item>
-                </div>
-              </div>
-
-              <div className="form-container">
-                <div className="form-pw">
-                  <Form.Item
-                    label="Password"
-                    name="password"
-                    rules={[{ required: true, message: "비밀번호를 입력해주세요!" }]}
-                  >
-                    <Input.Password 
-                    placeholder="비밀번호 입력"/>
-                  </Form.Item>
+                      <Input
+                        placeholder="이름 입력" />
+                    </Form.Item>
+                  </div>
+                  <div className="form-gender">
+                    <Form.Item name="gender" label="Gender" rules={[{ required: true, message: "성별을 선택해주세요!" }]}>
+                      <Select
+                        placeholder="성별"
+                        onChange={onGenderChange}
+                        allowClear
+                      >
+                        <Option value="male">남성</Option>
+                        <Option value="female">여성</Option>
+                      </Select>
+                    </Form.Item>
+                  </div>
                 </div>
 
-                <div className="form-phone">
-                  <Form.Item
-                    label="phoneNumber"
-                    name="phoneNumber"
-                    rules={[{ required: true, message: "폰 번호를 입력해주세요!" }]}
-                  >
-                    <Input 
-                    placeholder="01012345678"/>
-                  </Form.Item>
+                <div className="form-container">
+                  <div className="form-subject">
+                    <Form.Item
+                      label="학과"
+                      name="studentId"
+                      rules={[{ required: true, message: "학번을 입력해주세요!" }]}
+                    >
+                      <Input placeholder="전산학부" />
+                    </Form.Item>
+                  </div>
+                  <div className="form-studentnumber">
+                    <Form.Item
+                      label="학번"
+                      name="studentId"
+                      rules={[{ required: true, message: "학번을 입력해주세요!" }]}
+                    >
+                      <Input placeholder="12" />
+                    </Form.Item>
+                  </div>
                 </div>
-              </div>
-              <div className="form-container">
+
+                <div className="divider"/>
+
                 <div className="form-work">
                   <Form.Item
                     label="현재 소속 및 업무"
                     name="description"
                     rules={[{ required: true, message: "자기소개를 입력해주세요!" }]}
                   >
-                    <Input.TextArea />
+                    <Input placeholder="카이스트 대학원 신소재공학과" />
                   </Form.Item>
                 </div>
-              </div>
+                <div className="form-phone">
+                  <Form.Item
+                    label="phoneNumber"
+                    name="phoneNumber"
+                    rules={[{ required: true, message: "폰 번호를 입력해주세요!" }]}
+                  >
+                    <Input
+                      placeholder="01012345678" />
+                  </Form.Item>
+                </div>
+                <div className="form-email">
+                  <Form.Item
+                    label="Email"
+                    name="email"
+                    rules={[{ required: true, message: "이메일을 입력해주세요!" }]}
+                  >
+                    <Input
+                      placeholder="이메일" />
+                  </Form.Item>
+                </div>
+                <div className="form-pw">
+                  <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: "비밀번호를 입력해주세요!" }]}
+                  >
+                    <Input.Password
+                      placeholder="비밀번호 입력" />
+                  </Form.Item>
+                </div>
 
 
-              {isError && <Alert message="닉네임이 중복됩니다." type="error" />}
-              <div className="join-and-login">
-                {/* <Link to="/">
+
+                {isError && <Alert message="닉네임이 중복됩니다." type="error" />}
+                <div className="join-and-login">
+                  {/* <Link to="/">
                   <div className="link-to-login">로그인</div>
                 </Link> */}
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    회원가입
-                  </Button>
-                </Form.Item>
-              </div>
-            </Form>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                      회원가입
+                    </Button>
+                  </Form.Item>
+                </div>
+              </Form>
+            </>
+              : <>
+                <Form initialValues={{ remember: true }} onFinish={onFinish}>
+                  <Form.Item
+                    name="image"
+                    rules={[
+                      { required: true, message: "프로필 사진을 추가해주세요!" },
+                    ]}
+                  >
+                    <Upload
+                      name="image"
+                      listType="picture-card"
+                      className="avatar-uploader"
+                      showUploadList={false}
+                      action={
+                        "http://ec2-13-125-111-9.ap-northeast-2.compute.amazonaws.com/uploads"
+                      }
+                      onChange={handleChange}
+                    >
+                      {imageUrl ? (
+                        <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                      ) : (
+                        <div>
+                          <PlusOutlined style={{ color: "#00000070" }} />
+                          {/* <div style={{ marginTop: 8 , color:'#ffffff'}}>Profile</div> */}
+                        </div>
+                      )}
+                    </Upload>
+                  </Form.Item>
+
+                  {/* <Form.Item
+                label="Nickname"
+                name="nickname"
+                rules={[{ required: true, message: "닉네임을 입력해주세요!" }]}
+              >
+                <Input />
+              </Form.Item> */}
+
+                  <div className="form-container">
+                    <div className="form-name">
+                      <Form.Item
+                        label="이름"
+                        name="name"
+                        rules={[{ required: true, message: "이름을 입력해주세요!" }]}
+                      >
+                        <Input
+                          placeholder="이름 입력" />
+                      </Form.Item>
+                    </div>
+                    <div className="form-studentnumber">
+                      <Form.Item
+                        label="학번"
+                        name="studentId"
+                        rules={[{ required: true, message: "학번을 입력해주세요!" }]}
+                      >
+                        <Input placeholder="12" />
+                      </Form.Item>
+                    </div>
+                    <div className="form-subject">
+                      <Form.Item
+                        label="학과"
+                        name="studentId"
+                        rules={[{ required: true, message: "학번을 입력해주세요!" }]}
+                      >
+                        <Input placeholder="전산학부" />
+                      </Form.Item>
+                    </div>
+                  </div>
+
+                  <div className="form-container">
+                    <div className="form-email">
+                      <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[{ required: true, message: "이메일을 입력해주세요!" }]}
+                      >
+                        <Input
+                          placeholder="이메일" />
+                      </Form.Item>
+                    </div>
+                    <div className="form-gender">
+                      <Form.Item name="gender" label="Gender" rules={[{ required: true, message: "성별을 선택해주세요!" }]}>
+                        <Select
+                          placeholder="성별"
+                          onChange={onGenderChange}
+                          allowClear
+                        >
+                          <Option value="male">남성</Option>
+                          <Option value="female">여성</Option>
+                          <Option value="other">기타</Option>
+                        </Select>
+                      </Form.Item>
+                    </div>
+                  </div>
+
+                  <div className="form-container">
+                    <div className="form-pw">
+                      <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: "비밀번호를 입력해주세요!" }]}
+                      >
+                        <Input.Password
+                          placeholder="비밀번호 입력" />
+                      </Form.Item>
+                    </div>
+
+                    <div className="form-phone">
+                      <Form.Item
+                        label="phoneNumber"
+                        name="phoneNumber"
+                        rules={[{ required: true, message: "폰 번호를 입력해주세요!" }]}
+                      >
+                        <Input
+                          placeholder="01012345678" />
+                      </Form.Item>
+                    </div>
+                  </div>
+                  <div className="form-container">
+                    <div className="form-work">
+                      <Form.Item
+                        label="현재 소속 및 업무"
+                        name="description"
+                        rules={[{ required: true, message: "자기소개를 입력해주세요!" }]}
+                      >
+                        <Input.TextArea />
+                      </Form.Item>
+                    </div>
+                  </div>
+
+
+                  {isError && <Alert message="닉네임이 중복됩니다." type="error" />}
+                  <div className="join-and-login">
+                    {/* <Link to="/">
+                  <div className="link-to-login">로그인</div>
+                </Link> */}
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit">
+                        회원가입
+                      </Button>
+                    </Form.Item>
+                  </div>
+                </Form>
+              </>
+            }
+
           </div>
         </div>
 
