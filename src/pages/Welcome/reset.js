e2d60649f0caf986d48e4edbaec32f99e4214bc9
thestarkor <sk_and_mc@kaist.ac.kr>
2022-04-12 21:56:43 +0900

    비밀번호 변경

--- src/pages/Welcome/reset.js
+++ src/pages/Welcome/reset.js
@@ -2,24 +2,25 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
+import "./reset.scss"
+
 import { Form, Input, Button, Alert } from 'antd';
 import { Link } from 'react-router-dom';
 
-import "./login.scss";
-
-const Login = () => {
+const Reset = () => {
   const [, setCookie] = useCookies(['Authorization']);
   const [isLogging, setLogging] = useState(false);
   const [isError, setError] = useState(false);
 
   const onFinish = async (values) => {
-    const { email, password } = values;
+    const { email, password, studentId, name } = values;
     setLogging(true);
-    clickLogin();
     try {
-      const res = await axios.post('/users/login', {
+      const res = await axios.put('/users/reset_password', {
         email,
         password,
+        studentId,
+        name
       })
       setCookie('Authorization', res.data.token, { path: '/', maxAge: 24 * 60 * 60 });
       window.location.href = "/"
@@ -28,20 +29,7 @@ const Login = () => {
       setLogging(false);
       setError(true);
     }
-  }
 
-  const clickLogin = () => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: 'login'
-    })
-  }
-
-  const clickMoveJoin = () => {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      event: 'move_to_join'
-    })
   }
 
   return (
@@ -49,7 +37,7 @@ const Login = () => {
       <div className="login-container">
         <div className="login-box">
           <div className="greeting-text1">
-            로그인
+            비밀번호 변경
           </div>
           <div className="signup-form-container">
             <Form
@@ -68,13 +56,35 @@ const Login = () => {
                 </div>
               </div>
               <div className="form-container">
+                <div className="form-email">
+                  <Form.Item
+                    label="이름"
+                    name="name"
+                    rules={[{ required: true, message: '이름을 입력해주세요!' }]}
+                  >
+                    <Input placeholder="이름을 입력하세요" />
+                  </Form.Item>
+                </div>
+              </div>
+              <div className="form-container">
+                <div className="form-email">
+                  <Form.Item
+                    label="학번"
+                    name="studentId"
+                    rules={[{ required: true, message: '학번을 입력해주세요!' }]}
+                  >
+                    <Input placeholder="학번을 입력하세요. ex) 17" />
+                  </Form.Item>
+                </div>
+              </div>
+              <div className="form-container">
                 <div className="login-form-pw">
                   <Form.Item
-                    label="Password"
+                    label="새 비밀번호"
                     name="password"
                     rules={[{ required: true, message: '비밀번호를 입력해주세요!' }]}
                   >
-                    <Input.Password placeholder="비밀번호를 입력하세요" />
+                    <Input.Password placeholder="새 비밀번호를 입력하세요" />
                   </Form.Item>
                 </div>
               </div>
@@ -82,23 +92,12 @@ const Login = () => {
               {isError && <Alert message="이메일 혹은 비밀번호가 올바르지 않습니다." type="error" />}
               <div className="button">
                 <Form.Item>
-                  <Button type="primary" htmlType="submit" loading={isLogging} onClick={clickLogin}>
-                    로그인
+                  <Button type="primary" htmlType="submit" loading={isLogging}>
+                    비밀번호 변경
                   </Button>
                 </Form.Item>
               </div>
             </Form>
-          </div>
-          <div className="forget-button">
-            <Link to="/reset" className="text">
-              <div> 비밀번호를 잊으셨나요? </div>
-            </Link>
-          </div>
-          <div className="signup-button">
-            <div>밥한끼가 처음이신가요?</div>
-            <Link to="/join" className="text" onClick={clickMoveJoin}>
-              <div> 회원가입 </div>
-            </Link>
           </div>
         </div>
       </div>
@@ -106,4 +105,4 @@ const Login = () => {
   )
 }
 
-export default Login;
+export default Reset
